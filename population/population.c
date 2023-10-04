994b6c990a5c1c666d6359b971a17baa98ca1248
MTX <96997652+MTX77@users.noreply.github.com>
2023-10-04 20:11:07 +0200

    Update population.c

--- population/population.c
+++ population/population.c
@@ -7,25 +7,25 @@ int main(void)
     int end = 0;
     int years = 0;
 
-    // TODO: Prompt for start size
+    //Prompt for start size
     do
     {
         start = get_int("Start size: ");
     }
     while (start < 9);
 
-    // TODO: Prompt for end size
+    //Prompt for end size
     do
     {
         end = get_int("End size: ");
     }
     while (end < start);
-    // TODO: Calculate number of years until we reach threshold
+    //Calculate number of years until we reach threshold
     for (years = 0; start < end; years++)
     {
         start += (start / 3 - start / 4);
     }
 
-    // TODO: Print number of years
+    //Print number of years
     printf("Years: %i\n", years);
 }
